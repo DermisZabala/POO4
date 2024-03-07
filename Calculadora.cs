3310b3437969d527e96c8e886d9e3883290da5f1
DermisZabala <dermiszabala@gmail.com>
2024-03-06 21:16:52 -0400

    MODULARIZACION

---
+++ Calculadora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO4
+{
+    internal class Calculadora
+    {
+
+        public Calculadora(double sum1, double sum2)
+        {
+            double resultado = sum1 + sum2;
+            Console.WriteLine($"{sum1} + {sum2} = {resultado}");
+        }
+
+        public Calculadora(double mul1, double mul2, int i = 0)
+        {
+            double resultado = mul1 * mul2;
+            Console.WriteLine($"{mul1} * {mul2} = {resultado}");
+
+        }
+        public Calculadora(double div1, double div2, int i = 0, int i1 = 0)
+        {
+
+            double resultado = div1 / div2;
+
+            Console.WriteLine($"{div1} / {div2} = {resultado}");
+
+        }
+        public Calculadora(double resi1, double resi2, int i = 0, int i2 = 0, int i3 = 0)
+        {
+            double resultado = resi1 % resi2;
+
+            Console.WriteLine($"{resi1} % {resi2} = {resultado}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace POO4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            realizaTarea();
+
+            Console.ReadKey();
+        }
+
+        static void realizaTarea()
+        {
+            Console.WriteLine("¿Qué desea calcular? \n1. La suma de dos numeros " +
+                "\n2. La multiplicacion de dos numeros \n3. La division de dos numeros \n4. El resto de una division");
+
+            bool intentar = false;
+            
+            while (!intentar)
+            {
+                string op = Console.ReadLine();
+
+                switch (op)
+                {
+
+                    case "1":
+
+                        Console.WriteLine("Ingrese el n° uno");
+                        double sum1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el n° dos");
+                        double sum2 = double.Parse(Console.ReadLine());
+
+                        Calculadora suma = new Calculadora(sum1, sum2);
+
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Ingrese el n° uno");
+                        double multiplicacion1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el n° dos");
+                        double multiplicacion2 = double.Parse(Console.ReadLine());
+
+                        Calculadora multiplicacion = new Calculadora(multiplicacion1, multiplicacion2, 0);
+
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Ingrese el n° uno");
+                        double division1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el n° dos");
+                        double division2 = double.Parse(Console.ReadLine());
+
+                        Calculadora division = new Calculadora(division1, division2, 0, 0);
+
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Ingrese el n° uno");
+                        double residuo1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el n° dos");
+                        double residuo2 = double.Parse(Console.ReadLine());
+
+                        Calculadora residuo = new Calculadora(residuo1, residuo2, 0, 0, 0);
+
+                        break;
+                    default:
+                        Console.WriteLine("Has ingresado una opción incorrecta \nVuelva a intentarlo");
+                        
+                        break;
+                        
+                }
+
+                Console.WriteLine("Ingrese N para salir \nIngrese Y para continuar ");
+                string salir = Console.ReadLine().ToUpper();
+
+                if (salir == "Y")
+                {
+                    intentar = false;
+                    Console.WriteLine("1. La suma de dos numeros \n2. La multiplicacion de dos numeros \n3. La division de dos numeros \n4. El resto de una division");
+
+                }
+                else if(salir == "N")
+                {
+                    intentar = true;
+                }
+                
+
+            }
+        }
+    }
+}
